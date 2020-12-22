ec605aae2ceb9b1682af6b06a470d387d57636c9
Ricardo Haninec Silva <63988868+haninec@users.noreply.github.com>
2020-12-22 12:14:53 -0500

    Update environment.prod.ts
    ricardohs.ui.site

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     hmr: false,
     apiEndpoint: 'https://ricardo-hs.tk/api/',
-    tokenAuthStorage: 'ricardohs.ui.site.prod'
+    tokenAuthStorage: 'ricardohs.ui.site'
 };
